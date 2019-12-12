b2f0cde874ee7c0d49c8b6e6398da0b25989b6ca
Thomas Couchoud <thomas.couchoud@gmail.com>
2019-12-12 14:01:50 +0100

    Add CLI names

--- src/main/java/fr/raksrinana/youtubemp3updater/CLIParameters.java
+++ src/main/java/fr/raksrinana/youtubemp3updater/CLIParameters.java
@@ -10,16 +10,16 @@ import java.nio.file.Path;
 
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class CLIParameters{
-	@Parameter(description = "The path to the database containing the list of downloaded ids", converter = PathConverter.class)
+	@Parameter(names = {"--databasePath"}, description = "The path to the database containing the list of downloaded ids", converter = PathConverter.class)
 	@Getter
 	private Path databasePath = FileUtils.getAppDataFolder().resolve("YoutubeMP3Updater").resolve("downloads.db");
-	@Parameter(description = "The input file from the website", converter = PathConverter.class)
+	@Parameter(names = {"--input"}, description = "The input file from the website", converter = PathConverter.class)
 	@Getter
 	private Path inputPath;
-	@Parameter(description = "The folder where to put the downloaded mp3", converter = PathConverter.class)
+	@Parameter(names = {"--output"}, description = "The folder where to put the downloaded mp3", converter = PathConverter.class)
 	@Getter
 	private Path outputPath = FileUtils.getDesktopFolder().resolve("YTMP3");
-	@Parameter(description = "If set, this will delete IDs in the database instead of downloading them")
+	@Parameter(names = {"--delete"}, description = "If set, this will delete IDs in the database instead of downloading them")
 	@Getter
 	private boolean deleteInDb;
 }
