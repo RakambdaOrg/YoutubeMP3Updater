5b34188266093300d9050d08597c7992d7e34a44
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-04-27 09:40:32 +0200

    Use kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+    idea
+    java
+    application
+    id("com.github.johnrengelman.shadow") version ("6.1.0")
+    id("com.github.ben-manes.versions") version ("0.38.0")
+    id("io.freefair.lombok") version ("6.0.0-m2")
+    id("com.gorylenko.gradle-git-properties") version ("2.3.1-rc3")
+}
+
+group = "fr.raksrinana"
+description = "YoutubeMP3Updater"
+
+dependencies {
+    implementation(libs.slf4j)
+    implementation(libs.bundles.log4j2)
+
+    implementation(libs.bundles.raksrinanaUtils)
+
+    implementation(libs.bundles.jackson)
+
+    implementation(libs.unirest)
+    implementation(libs.picocli)
+
+    compileOnly(libs.jetbrainsAnnotations)
+}
+
+repositories {
+    val githubRepoUsername: String by project
+    val githubRepoPassword: String by project
+
+    maven {
+        url = uri("https://maven.pkg.github.com/RakSrinaNa/JavaUtils/")
+        credentials {
+            username = githubRepoUsername
+            password = githubRepoPassword
+        }
+    }
+    mavenCentral()
+}
+
+tasks {
+    processResources {
+        expand(project.properties)
+    }
+
+    compileJava {
+        val moduleName: String by project
+        inputs.property("moduleName", moduleName)
+
+        options.encoding = "UTF-8"
+        options.isDeprecation = true
+
+        doFirst {
+            val compilerArgs = options.compilerArgs
+            compilerArgs.add("--module-path")
+            compilerArgs.add(classpath.asPath)
+            classpath = files()
+        }
+    }
+
+    jar {
+        manifest {
+            attributes["Multi-Release"] = "true"
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    shadowJar {
+        archiveBaseName.set(project.name)
+        archiveClassifier.set("shaded")
+        archiveVersion.set("")
+    }
+
+    wrapper {
+        val wrapperVersion: String by project
+        gradleVersion = wrapperVersion
+    }
+}
+
+application {
+    val moduleName: String by project
+    val className: String by project
+
+    mainClassName = className
+    mainModule.set(moduleName)
+    mainClass.set(className)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "youtubemp3updater"
+
+enableFeaturePreview("VERSION_CATALOGS")
